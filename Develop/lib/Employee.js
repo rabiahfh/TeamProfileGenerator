9d7d29c7bb1333ab867f0234ac8f8354e48beee2
Rabiah Hogans <hogansrabiah45@gmail.com>
2021-02-23 16:14:29 -0500

    feat:defined employee

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,27 @@
+// defined employee class
+class Employee {
+    constructor(name,email, id, role){
+        this.name = name
+        this.email = email
+        this.id = id
+        this.role = role
+
+    }
+
+    getName(){
+        return this.name
+    }
+    
+    getEmail(){
+        return this.email
+    }
+    getId(){
+        return this.id
+    }
+    getRole(){
+        return this.role
+    }
+    
+    
+}
+module.exports = Employee;

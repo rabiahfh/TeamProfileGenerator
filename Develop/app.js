50cee57c29da2e14ce3eeb572df566cb6dceded2
Rabiah Hogans <hogansrabiah45@gmail.com>
2021-02-24 22:48:02 -0500

    feat: added getintern to app.js

--- Develop/app.js
+++ Develop/app.js
@@ -99,7 +99,6 @@ function app() {
                         })
         
                 }
-// Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required
